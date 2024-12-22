40732079c5c083f2c569e727c9747287ddfab422
Maxim <max@MacBook-Pro-Maxim.local>
2024-12-22 17:45:13 +0200

    legend

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import Header from "./components/Header.vue";
+
+import Main from "./views/Main.vue";
+</script>
+
+<template>
+  <main>
+    <Header />
+    <Main />
+  </main>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
